845f3389f32b9e29cc8f215499506a8abdf5d300
FazleRahmanEvan <evanshakib26@gmail.com>
2022-02-16 14:42:40 +0600

    first commit

---
+++ js/login.js
@@ -0,0 +1,17 @@
+document.getElementById('login-submit').addEventListener('click', function(){
+    console.log('button clicked');
+    const emailField = document.getElementById('user-email');
+    const userEmail = emailField.value;
+
+    const passwordField = document.getElementById('user-password');
+    const userPassword = passwordField.value;
+
+    if(userEmail == 'sontan@gmail.com' &&
+    userPassword =='secret'){
+        window.location.href = 'banking.html';
+      
+    }
+})
+
+
+
